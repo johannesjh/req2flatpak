dec97120f13730e5fcba374ee1c58fe8fd898f5a
johannesjh <johannesjh@users.noreply.github.com>
2022-10-25 19:30:38 +0200

    uses sphinx rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,5 +31,4 @@ exclude_patterns = []
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 # =============================================================================
 
-# html_theme = "sphinx_rtd_theme_github_versions"
-# html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme_github_versions"
