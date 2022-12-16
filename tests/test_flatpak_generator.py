c4a12b0783d9ab37f214a0b379638f94215a9669
johannesjh <johannesjh@users.noreply.github.com>
2022-12-16 13:22:29 +0100

    sorts sources to make the script's output consistent.
    motivation: repeated runs of req2flatpak should produce the same output.
    my experience however was that the sort order differed for whatever reason.
    this change ensures that all sources are sorted in a consistent way.

--- tests/test_flatpak_generator.py
+++ tests/test_flatpak_generator.py
@@ -16,11 +16,15 @@ class ExampleUsageTest(unittest.TestCase):
     # pylint: disable=line-too-long
     downloads = {
         Download(
+            package="scikit-learn",
+            version="1.1.3",
             filename="scikit_learn-1.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
             url="https://files.pythonhosted.org/packages/23/b6/5d339516e3fbb6cde8ad87e85d9f17a3270c9e508c860785f0b6239ea33a/scikit_learn-1.1.3-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl",
             sha256="701181792a28c82fecae12adb5d15d0ecf57bffab7cf4bdbb52c7b3fd428d540",
         ),
         Download(
+            package="requests",
+            version="2.28.1",
             filename="requests-2.28.1-py3-none-any.whl",
             url="https://files.pythonhosted.org/packages/ca/91/6d9b8ccacd0412c08820f72cebaa4f0c0441b5cda699c90f618b6f8a1b42/requests-2.28.1-py3-none-any.whl",
             sha256="8fefa2a1a1365bf5520aac41836fbee479da67864514bdb821f31ce07ce65349",
